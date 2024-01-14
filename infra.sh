871c9ca06bf5b8f9c1e320d31b4ed26865bdae5f
hmoradirad <moradirad007@gmail.com>
2024-01-14 16:17:47 +0330

    adding script for run terraform and ansible

---
+++ infra.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+git clone https://github.com/HMoradiRad/ceph-cluster.git
+
+cd ceph-cluster
+
+terraform apply -auto-approve
+echo "go to ceph1 ssh ro other server"
+sleep 20
+ansible-playbook -i inventory/hosts.ini playbook.yml  --ask-vault-pass
